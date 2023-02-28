b0f205ec45b60a7d76fd5b6a5b7194e65e0ad067
Andrey_BespaloFF <andrey2802@mail.ru>
2023-02-28 10:51:26 +0300

    secondDigit of number

---
+++ S2WH2_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Hello, friend");
+Console.Write("Enter your number:   ");
+int number = Convert.ToInt32(Console.ReadLine());
+int firsrdigit = number/10;
+int resultfirstdigit = firsrdigit%10;
+Console.WriteLine(resultfirstdigit);
+
+
+
