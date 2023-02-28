f6798f9542c1e672e1aa159b919a7d40c2360a7c
Andrey_BespaloFF <andrey2802@mail.ru>
2023-02-28 17:53:44 +0300

    work/holidaн

--- S2WH2_2/Program.cs
+++ S2WH2_2/Program.cs
@@ -7,4 +7,3 @@
 Console.Write("Hello,it's you?! Ok, enter your number:  ");
 int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(number > 99 ? number.ToString()[2] : "no digit");
-
---
+++ S2WH2_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+//  обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+string[] days = { "Monday", "Tuesday", "Wenesday", "Thursday", "Friday", "Saturday", "Sunday" };
+Console.Write("Hello,it's you?! Ok, enter your daynumber:  ");
+int daynumber = Convert.ToInt32(Console.ReadLine());
+if (daynumber > 7 | daynumber < 1)
+{
+    Console.WriteLine("God did not invent such a day");
+}
+if (daynumber <= 5)
+{
+    Console.WriteLine(days[daynumber - 1]);
+    Console.WriteLine("Work Hard");
+}
+if (daynumber == 6 | daynumber == 7)
+{
+    Console.WriteLine(days[daynumber - 1]);
+    Console.WriteLine("Yes!!! It's holiday, let's go party");
+}
