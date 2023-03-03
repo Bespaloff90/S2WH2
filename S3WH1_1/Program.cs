4d89d063f35d71f3719131832940dc82ffd4ccb5
Andrey_BespaloFF <andrey2802@mail.ru>
2023-03-03 14:06:07 +0300

    palyndrom

---
+++ S3WH1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход 
+// ятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+  Console.Write("Enter the number polyndrom:  ");
+            int number = int.Parse(Console.ReadLine());
+            int pal = 0, num = number;
+            while (number > 0) {pal = pal * 10 + number % 10; number /= 10;}
+            Console.WriteLine(num == pal ? "палиндром" : "не палиндром");
